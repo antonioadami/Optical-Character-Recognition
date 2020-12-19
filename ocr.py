52d9c5682195d4516fc9ac20536c3745ec3a7f94
Antonio Adami <antonioadamipalma@gmail.com>
2020-12-19 17:42:36 -0300

    Atualização código

--- ocr.py
+++ ocr.py
@@ -3,32 +3,6 @@
 import cv2
 import pytesseract
 
-class Output:
-    BYTES = 'bytes'
-    DATAFRAME = 'data.frame'
-    DICT = 'dict'
-    STRING = 'string'
-    
-
-def image_to_string(
-    image,
-    lang=None,
-    config='',
-    nice=0,
-    output_type=Output.STRING,
-    timeout=0,
-):
-    """
-    Retorna o resultado de um Tesseract OCR
-    """
-    args = [image, 'txt', lang, config, nice, timeout]
-
-    return {
-        Output.BYTES: lambda: pytesseract.run_and_get_output(*(args + [True])),
-        Output.DICT: lambda: {'text': pytesseract.run_and_get_output(*args)},
-        Output.STRING: lambda: pytesseract.run_and_get_output(*args),
-    }[output_type]()
-
 # Lendo a imagem
 image = cv2.imread('texto.jpg')
 
@@ -38,7 +12,7 @@ gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 gray = cv2.bitwise_not(img_bin)
 
 # Convertendo para texto
-text = image_to_string(gray)
+text = pytesseract.image_to_string(gray)
 
 print(text)
 
